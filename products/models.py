f13b1dc6c37a0dae7ddef21605533f35288e6436
Mark Lewis <markblewis2008@gmail.com>
2023-05-16 20:34:02 -0700

    initial commit

---
+++ products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
+# Create your models here.
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
